8afd6082d8a00093c0302635960d1f1796c26c8f
jihoo <134384139+jihoo12@users.noreply.github.com>
2026-09-25 15:18:35 +0900

    feat: expand Lua runtime scripting API

--- src/modules/lua/lua.h
+++ src/modules/lua/lua.h
@@ -1,7 +1,12 @@
 #ifndef SHADY_MODULE_LUA_H
 #define SHADY_MODULE_LUA_H
 #include <stdbool.h>
+#include <stdint.h>
+#include <xkbcommon/xkbcommon.h>
+struct shady_toplevel;
 struct shady_server;
 bool shady_lua_init(struct shady_server *server);
 void shady_lua_fini(struct shady_server *server);
+void shady_lua_emit(struct shady_server *server,const char *event,struct shady_toplevel *toplevel);
+bool shady_lua_handle_key(struct shady_server *server,xkb_keysym_t sym,uint32_t modifiers);
 #endif
