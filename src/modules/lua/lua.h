8243be0ceceba6ca6167320206ec047de768463f
jihoo <134384139+jihoo12@users.noreply.github.com>
2026-09-25 15:11:21 +0900

    fix: include bool type in Lua module header

--- src/modules/lua/lua.h
+++ src/modules/lua/lua.h
@@ -1,5 +1,6 @@
 #ifndef SHADY_MODULE_LUA_H
 #define SHADY_MODULE_LUA_H
+#include <stdbool.h>
 struct shady_server;
 bool shady_lua_init(struct shady_server *server);
 void shady_lua_fini(struct shady_server *server);
