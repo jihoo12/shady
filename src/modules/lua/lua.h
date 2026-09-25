7df28384894793abcc36142f033036b2321ab549
jihoo <134384139+jihoo12@users.noreply.github.com>
2026-09-25 15:04:34 +0900

    feat: embed Lua 5.4 runtime

---
+++ src/modules/lua/lua.h
@@ -0,0 +1,6 @@
+#ifndef SHADY_MODULE_LUA_H
+#define SHADY_MODULE_LUA_H
+struct shady_server;
+bool shady_lua_init(struct shady_server *server);
+void shady_lua_fini(struct shady_server *server);
+#endif
